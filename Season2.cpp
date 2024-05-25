2e4065410f8eb061bf5608adbda0bd1feff4c4c7
Vladislav Sychugin <4myebay@mail.ru>
2024-05-25 14:27:05 +0500

    Добавьте файлы проекта.

---
+++ Season2.cpp
@@ -0,0 +1,23 @@
+﻿// Season2.cpp: С использованием ifdef/endif.
+
+#include "Season2.h"
+#include <iostream>
+
+#define SUMMER
+
+int main()
+{
+#ifdef WINTER
+	std::cout << "Winter." << '\n';
+#endif
+#ifdef SPRING
+	std::cout << "Spring." << '\n';
+#endif
+#ifdef SUMMER
+	std::cout << "Summer." << '\n';
+#endif
+#ifdef AUTUMN
+	std::cout << "Autumn." << '\n';
+#endif
+	return 0;
+}
